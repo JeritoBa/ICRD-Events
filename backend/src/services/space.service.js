96e526f528184beb0af6c6a00386e421f5b59155
SaraCA12 <scalderon@unal.edu.co>
2026-03-09 15:16:38 -0500

    Services and controllers space

--- backend/src/services/space.service.js
+++ backend/src/services/space.service.js
@@ -1,5 +1,7 @@
 import {pool} from '../db/sql.js'
 
+
+// Function to Show all spaces
 export async function getSpaces() {
     const query = `select  s.name, s.description, s.status, sc.name 
                     from public.space s 
@@ -9,7 +11,8 @@ export async function getSpaces() {
     return rows;
 }
 
-export async function getSpace(name) {
+//Function to filter by name (search bar)
+export async function getSpaceByName(name) {
     const query = `select  s.name, s.description, s.status, sc.name as scenario
                     from public.space s 
                     inner join public.scenario sc 
@@ -19,41 +22,38 @@ export async function getSpace(name) {
     return rows
 }
 
-export const createSpace = async (req,res) =>{
-    //datos que ingresa el cliente
-    const {name, birth_date} = req.body
-    const query = 'INSERT INTO test.patient (name, birth_date) VALUES ($1,$2) RETURNING *'
-
-    try{
-        const response = await pool.query(query, [name, birth_date]);
-        res.status(201).json({ message: 'Patient created successfully'})
-    } catch (error){
-        console.error('Error creating the patient: ', error)
-        res.status(404).json({message: 'Error creating the patient'})
-    }
+//Function to filter by status
+export async function getSpaceByStatus(status) {
+    const query = `select  s.name, s.description, s.status, sc.name as scenario
+                    from public.space s 
+                    inner join public.scenario sc 
+                    on sc.id = s.scenario_id
+                    where s.status = $1`
+    const {rows} = await pool.query(query, [status]);
+    return rows
 }
 
-export const deletePatient = async (req,res) =>{
-    const {id} = req.params;
-    const query = 'DELETE FROM test.patient WHERE id=$1'
-
-    try{
-        const response = await pool.query(query, [id]);
-        if (response.rowCount ===0){
-            return res.status(404).json({message: 'Patient not found'})
-        }
-
-        res.json({
-            message: 'Patient deletd succesfully',
-            patient: response.rows[0]
-        });
-    } catch (error){
-        console.error('Error deleting the patient: ', error)
-        res.status(404).json({message: 'Internal server error'})
-    }
+//Function to create a new space
+export async function createSpace(name, description, scenario) {
+    const query =`insert into public.space (name, description, scenario_id)
+                    values ($1, $2,(select sc.id from scenario sc where sc.name = $3 ))
+                    RETURNING *`
+    const {rows} = await pool.query(query,[name, description,scenario])
+    return rows[0]
 }
 
-export const updatePatient =async (req,res) =>{
+//Function to delete a space
+export async function deleteSpace (name,scenario_name){
+    const query = `DELETE FROM public.space 
+                    WHERE name=$1 and scenario_id=(select sc.id from scenario sc where sc.name = $2 )
+                    RETURNING *`
+    const {rows} = await pool.query(query, [name, scenario_name]);
+    return rows[0] || null
+}
+
+
+//Function to update an space
+async const updateSpace =async (req,res) =>{
     const {id} = req.params;
     const {name, birth_date} = req.body
 
